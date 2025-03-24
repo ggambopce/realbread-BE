d4f708500fb12d84f51790ca38b597e879c6a35d
ggambopce <ggambopce@gmail.com>
2025-03-25 08:08:41 +0900

    🎉 리얼빵집 시작

---
+++ src/main/java/com/jino/realbread/RealbreadApplication.java
@@ -0,0 +1,13 @@
+package com.jino.realbread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealbreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealbreadApplication.class, args);
+	}
+
+}

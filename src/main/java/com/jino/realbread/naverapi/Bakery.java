53e9016697dc38a7ced939516e4877f8bf8f7d69
ggambopce <ggambopce@gmail.com>
2025-03-25 09:14:07 +0900

    ✨ 네이버 검색 API

---
+++ src/main/java/com/jino/realbread/naverapi/Bakery.java
@@ -0,0 +1,28 @@
+package com.jino.realbread.naverapi;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bakery")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Bakery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String category;
+    private String address;
+    private String roadAddress;
+    private String link;
+    private String mapx;
+    private String mapy;
+}
---
+++ src/main/java/com/jino/realbread/naverapi/BakeryRepository.java
@@ -0,0 +1,8 @@
+package com.jino.realbread.naverapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BakeryRepository extends JpaRepository<Bakery, Long> {
+
+
+}

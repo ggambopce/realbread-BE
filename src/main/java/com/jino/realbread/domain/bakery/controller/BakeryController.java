8d39d38e761338811c3ab73d87ac9f88da5722e5
ggambopce <ggambopce@gmail.com>
2025-05-01 18:01:50 +0900

    ✨ 빵집 상세 컨트롤러

--- src/main/java/com/jino/realbread/domain/bakery/controller/BakeryController.java
+++ src/main/java/com/jino/realbread/domain/bakery/controller/BakeryController.java
@@ -1,21 +1,29 @@
 package com.jino.realbread.domain.bakery.controller;
 
+import com.jino.realbread.domain.bakery.dto.response.GetBakeryResponseDto;
+import com.jino.realbread.domain.bakery.service.BakeryService;
 import com.jino.realbread.naverapi.NaverSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/bakery")
 @RequiredArgsConstructor
 public class BakeryController {
 
     private final NaverSearchService naverSearchService;
+    private final BakeryService bakeryService;
 
-    @GetMapping("/bakery/search")
+    @GetMapping("/search")
     public ResponseEntity<String> searchAndSave(@RequestParam String keyword) {
         naverSearchService.searchAndSave(keyword);
         return ResponseEntity.ok("저장 완료");
+    }
+
+    @GetMapping("/{bakeryNumber}")
+    public ResponseEntity<? super GetBakeryResponseDto> getBakery(@PathVariable("bakeryNumber") Integer bakeryNumber) {
+        ResponseEntity<? super GetBakeryResponseDto> response = bakeryService.getBakery(bakeryNumber);
+        return response;
     }
 }

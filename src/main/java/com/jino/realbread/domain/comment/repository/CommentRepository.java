bc48f7b4054318827671b7520eb0306db07bb037
ggambopce <ggambopce@gmail.com>
2025-05-14 16:42:08 +0900

    ✨ 리뷰 리스트 레포지토리

--- src/main/java/com/jino/realbread/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/jino/realbread/domain/comment/repository/CommentRepository.java
@@ -1,11 +1,28 @@
 package com.jino.realbread.domain.comment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jino.realbread.domain.comment.entity.CommentEntity;
+import com.jino.realbread.domain.comment.repository.resultSet.GetCommentListResultSet;
 
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
+
+    @Query(value = "SELECT " +
+            "U.nickname AS nickname, " +
+            "U.profile_image AS profileImage, " +
+            "C.write_datetime AS writeDatetime, " +
+            "C.choice_menu AS choiceMenu, " +
+            "C.content AS content " +
+            "FROM comment AS C " +
+            "INNER JOIN user AS U " +
+            "ON C.user_id = U.id " +
+            "WHERE C.bakery_number = ?1 " +
+            "ORDER BY writeDatetime DESC", nativeQuery = true)
+    List<GetCommentListResultSet> getCommentList(Integer boardNumber);
 
 }

73eb2af0276dbb881a0384e3ab5662ef717a8cc0
ggambopce <ggambopce@gmail.com>
2025-05-14 18:09:47 +0900

    ✨ 빵집 리스트 뷰 생성

---
+++ src/main/java/com/jino/realbread/domain/view/BakeryListViewEntity.java
@@ -0,0 +1,69 @@
+package com.jino.realbread.domain.view;
+
+import org.hibernate.annotations.Immutable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Immutable
+@Entity(name = "bakery_list_view")
+@Table(name = "bakery_list_view")
+public class BakeryListViewEntity {
+
+    @Id
+    @Column(name = "menu_id")
+    private Integer menuId;
+
+    // Bakery 정보
+    @Column(name = "bakery_id")
+    private Integer bakeryId;
+
+    @Column(name = "bakery_title")
+    private String bakeryTitle;
+
+    @Column(name = "bakery_category")
+    private String bakeryCategory;
+
+    @Column(name = "bakery_address")
+    private String bakeryAddress;
+
+    @Column(name = "bakery_road_address")
+    private String bakeryRoadAddress;
+
+    @Column(name = "bakery_link")
+    private String bakeryLink;
+
+    @Column(name = "bakery_mapx")
+    private String bakeryMapx;
+
+    @Column(name = "bakery_mapy")
+    private String bakeryMapy;
+
+    @Column(name = "bakery_favorite_count")
+    private int bakeryFavoriteCount;
+
+    @Column(name = "bakery_comment_count")
+    private int bakeryCommentCount;
+
+    // Menu 정보
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private String menuPrice;
+
+    @Column(name = "menu_image_url")
+    private String menuImageUrl;
+
+    @Column(name = "menu_description", length = 2000)
+    private String menuDescription;
+
+}

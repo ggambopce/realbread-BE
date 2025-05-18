1e258b8fbaa83fb799517138c996c8adb99bc427
ggambopce <ggambopce@gmail.com>
2025-05-18 13:31:03 +0900

    ✨ 사용자 정보 응답 DTO

--- src/main/java/com/jino/realbread/domain/user/service/UserService.java
+++ src/main/java/com/jino/realbread/domain/user/service/UserService.java
@@ -2,8 +2,9 @@ package com.jino.realbread.domain.user.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.jino.realbread.domain.user.dto.response.GetSignInUserResponseDto;
 import com.jino.realbread.global.security.auth.PrincipalDetails;
 
 public interface UserService {
-    ResponseEntity<GetSignInUserResponseDto> getSignInUser(PrincipalDetails principalDetails);
+    ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(PrincipalDetails principalDetails);
 }
--- src/main/java/com/jino/realbread/domain/user/service/implement/UserServiceImplement.java
+++ src/main/java/com/jino/realbread/domain/user/service/implement/UserServiceImplement.java
@@ -1,8 +1,11 @@
 package com.jino.realbread.domain.user.service.implement;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.jino.realbread.domain.user.dto.response.GetSignInUserResponseDto;
 import com.jino.realbread.domain.user.service.UserService;
+import com.jino.realbread.global.dto.response.ResponseDto;
 import com.jino.realbread.global.security.auth.PrincipalDetails;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,13 @@ public class UserServiceImplement implements UserService {
     @Override
     public ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(PrincipalDetails principalDetails) {
 
+        try {
+            
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return GetSignInUserResponseDto.success()
     }
 
 }
